b22f183349dd7d03a4eef6f0add4cc27d1437a81
Jeet Z. H. Khondker <jeetzhkhondker@CoreBit.local>
2026-05-17 21:15:17 +0900

    fix (chore) : Added Missing type-check Script in Frontend Package File & Fixed the TypeScript Jest Configuration Conflict

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,11 +2,9 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the Path to your NextJS Application to Load "next.config.js" & ".env" Files in your Test Environment
   dir: "./",
 });
 
-// Add Any Custom Configurations to be Passed ✅ to Jest 🚀
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
@@ -21,7 +19,6 @@ const config: Config = {
     "!src/**/*.stories.{js,jsx,ts,tsx}",
     "!src/**/__tests__/**",
   ],
-};
+} as const;
 
-// "createJestConfig" is Exported this way to ensure that "next/jest" can load the Async NextJS Configuration
 export default createJestConfig(config);
